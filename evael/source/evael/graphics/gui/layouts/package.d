d6f3b6973bab781c5470eda7368972dae4c143a0
Robert Georges <robert.georges@dalenys.com>
2020-02-07 16:28:57 +0100

    Removing old dir

--- evael/source/evael/graphics/gui/layouts/package.d
+++
@@ -1,7 +0,0 @@
-module evael.graphics.gui.layouts;
-
-public 
-{
-	import evael.graphics.gui.layouts.layout;
-	import evael.graphics.gui.layouts.layout_params;
-}
--- evael/source/evael/physics/physics_2d/rigid_body_2d.d
+++
@@ -1,8 +0,0 @@
-module evael.physics.physics_2d.rigid_body;
-
-// import chipmunk;
-
-class RigidBody2D
-{
-	
-}
